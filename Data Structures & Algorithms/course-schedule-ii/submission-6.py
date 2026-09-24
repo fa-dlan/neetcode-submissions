cdb8c198b639bc899eefe32dc87f98c70a05b638
fadhlan k <138226358+fa-dlan@users.noreply.github.com>
2026-09-24 16:50:07 +0700

    Add: course-schedule-ii - submission-6

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-6.py
@@ -0,0 +1,53 @@
+from collections import deque
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        states = [0] * numCourses
+        neighbors = {}
+        for a, b in prerequisites:
+            if b not in neighbors:
+                neighbors[b] = [a]
+            else:
+                neighbors[b].append(a)
+        
+        def is_possible(numCourses, prerequisites):
+            def is_dag_from_node(node):
+                if states[node] == 1:
+                    return False
+                elif states[node] == 2:
+                    return True
+                states[node] = 1
+                if node in neighbors:
+                    for neighbor in neighbors[node]:
+                        if not is_dag_from_node(neighbor):
+                            return False
+                states[node] = 2
+                return True
+
+            for node in range(numCourses):
+                if states[node] == 0:
+                    if not is_dag_from_node(node):
+                        return False
+            return True
+        
+        if not is_possible(numCourses, prerequisites):
+            return []
+
+        out = deque()
+        visited = set()
+
+        def dfs(node):
+            if node in visited:
+                return
+            else:
+                visited.add(node)
+            if node in neighbors:
+                for neighbor in neighbors[node]:
+                    dfs(neighbor)
+            out.appendleft(node)
+
+        for node in range(numCourses):
+            dfs(node)
+            
+        return list(out)
+        
